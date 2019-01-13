ca1efc161549269dc60cd5c6b9704777f23ba5bd
司仲夏 <teamunion@aliyun.com>
2019-01-14 00:22:34 +0800

    Update api.js

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
-const ApiUrl = 'http://192.168.31.144:9091';
+// const ApiUrl = 'http://192.168.31.144:9091';
 // const ApiUrl = 'http://192.168.31.96:9091';
 // const ApiUrl = 'http://192.168.1.107:9091';
-// const ApiUrl = 'https://wxapi.yeetong.cn';
+const ApiUrl = 'https://wxapi.yeetong.cn';
 module.exports = {
 
   // 微信登陆
