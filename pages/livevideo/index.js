a23e4b04a277c1581aecfc9a3d9a81691a7b7ce3
sizhongxia <sizhongxia@haijupai.com>
2019-01-14 19:00:17 +0800

    直播操作

--- pages/livevideo/index.js
+++ pages/livevideo/index.js
@@ -7,21 +7,31 @@ Page({
     CustomBar: app.globalData.CustomBar,
     socketConnect: false,
     topicState: false,
+    lastOptionTxt: '未进行任何操作'
+  },
+  setOptionTxt: function(txt) {
+    this.setData({
+      lastOptionTxt: txt
+    });
   },
   onLoad: function () {
     const _this = this;
     _this.lpc = wx.createLivePlayerContext('rtmpPlayId');
     wx.onSocketError(function () {
       logger.log('连接 wss://wxapi.yeetong.cn/socket 失败');
+      _this.setOptionTxt('连接 wss://wxapi.yeetong.cn/socket 失败')
     });
     wx.onSocketOpen(function () {
+      _this.setOptionTxt('onSocketOpen, 连接成功');
       _this.setData({
         socketConnect: true
       });
     });
     wx.onSocketMessage(function (data) {
+      _this.setOptionTxt('接收到消息');
       logger.log(data);
       if (data.data === '#topic#true#') {
+        _this.setOptionTxt('订阅成功');
         // 订阅成功
         _this.setData({
           topicState: true
@@ -33,28 +43,37 @@ Page({
         socketConnect: false
       });
       logger.log('Socket连接关闭');
+      _this.setOptionTxt('Socket连接关闭');
     });
   },
   toConnectSocket: function () {
+    const _this = this;
+    _this.setOptionTxt('连接Socket...');
     // 连接Socket
     wx.connectSocket({
       url: 'wss://wxapi.yeetong.cn/socket'
     });
   },
   toTopic: function () {
+    const _this = this;
+    _this.setOptionTxt('发送订阅请求...');
     // 发送订阅请求
     this.sendSocketData("#topic#add#VSG/5c2045427e298eb6d157ca7a/5c2045427e298eb6d157ca7a/PushFlowStatus#WXXCX_1#");
   },
   toPullStream: function () {
-    if (!this.data.topicState) {
+    const _this = this;
+    if (!_this.data.topicState) {
       return false;
     }
+    _this.setOptionTxt('发送拉流请求');
     // 发送拉流请求
     this.sendSocketData("#send_data#VSG/5c2045427e298eb6d157ca7a/Call#S&5c2045427e298eb6d157ca7a&1&20190113152910&VSG&E#WXXCX_1#");
   },
   rtmpPlay: function () {
-    if (lpc) {
-      lpc.play({
+    const _this = this;
+    if (_this.lpc) {
+      _this.setOptionTxt('播放');
+      _this.lpc.play({
         success: function (err) {
           wx.showToast({
             title: '播放',
@@ -68,8 +87,9 @@ Page({
     }
   },
   rtmpPause: function () {
-    if (lpc) {
-      lpc.pause({
+    const _this = this;
+    if (_this.lpc) {
+      _this.lpc.pause({
         success: function (err) {
           wx.showToast({
             title: '暂停',
@@ -83,8 +103,9 @@ Page({
     }
   },
   rtmpResume: function () {
-    if (lpc) {
-      lpc.resume({
+    const _this = this;
+    if (_this.lpc) {
+      _this.lpc.resume({
         success: function (err) {
           wx.showToast({
             title: '恢复',
@@ -98,8 +119,9 @@ Page({
     }
   },
   rtmpFullScreen: function () {
-    if (lpc) {
-      lpc.requestFullScreen({
+    const _this = this;
+    if (_this.lpc) {
+      _this.lpc.requestFullScreen({
         success: function (err) {
           wx.showToast({
             title: '全屏',
@@ -113,8 +135,9 @@ Page({
     }
   },
   rtmpExitFullScreen: function () {
-    if (lpc) {
-      lpc.exitFullScreen({
+    const _this = this;
+    if (_this.lpc) {
+      _this.lpc.exitFullScreen({
         success: function (err) {
           wx.showToast({
             title: '退出全屏',
