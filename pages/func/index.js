f4a8217cd1a440b8d95fd7bb872a21af04dc058f
夏风飞舞 <1217491317@qq.com>
2019-03-08 00:41:36 +0800

    修改

--- pages/func/index.js
+++ pages/func/index.js
@@ -1,5 +1,5 @@
-var farmService = require('../../../service/farm.js');
-var util = require('../../../utils/util.js');
+var farmService = require('../../service/farm.js');
+var util = require('../../utils/util.js');
 const app = getApp()
 
 Page({
