66ee4517dfff34a8c012ad889e7954e97f317255
sizhongxia <sizhongxia@haijupai.com>
2019-03-07 15:09:42 +0800

    Update

--- pages/console/index.js
+++ pages/console/index.js
@@ -1,13 +1,40 @@
+var farmService = require('../../service/farm.js');
 const app = getApp();
 
 Page({
   data: {
-    currentFarmIdentity: ''
+    currentFarmId: '',
+    currentFarmIdentity: '',
+    funcs: []
   },
   onLoad: function () {
     const _this = this;
     _this.setData({
+      currentFarmId: wx.getStorageSync('curr-farm-id'),
       currentFarmIdentity: wx.getStorageSync('curr-farm-identity')
+    });
+  },
+  onShow: function () {
+    const _this = this;
+    wx.showLoading({
+      title: '请稍后...',
+      mask: true
+    });
+    farmService.getFarmFuncs(_this.data.currentFarmId).then(res => {
+      wx.hideLoading();
+      _this.setData({
+        funcs: res
+      });
+    }).catch(err => {
+      wx.hideLoading();
+      _this.setData({
+        funcs: []
+      });
+      if (err) {
+        if (err.message) {
+          util.showErrorToast(err.message);
+        }
+      }
     });
   }
 })
